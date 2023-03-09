e644f04d3b551a5ccca1669ce3f4214c940d489e
Wesley Feldsine <laocoon7@gmail.com>
2023-03-09 15:12:02 -0800

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
 #![warn(missing_docs)]
+#![doc = include_str!("../README.md")]
 
 #[cfg(feature = "point")]
 /// Contains the [`Point`] structure and related methods
 pub mod point;
 #[cfg(feature = "ray")]
+/// Contains the [`Ray`] structure and related methods
 pub mod ray;
 #[cfg(feature = "line")]
+/// Contains the [`Line`] structure and related methods
 pub mod line;
 #[cfg(feature = "triangle")]
+/// Contains the [`Triangle`] structure and related methods
 pub mod triangle;
 #[cfg(feature = "rectangle")]
+/// Contains the [`Rectangle`] structure and related methods
 pub mod rectangle;
 #[cfg(feature = "circle")]
+/// Contains the [`Circle`] structure and related methods
 pub mod circle;
 #[cfg(feature = "ellipse")]
+/// Contains the [`Ellipse`] structure and related methods
 pub mod ellipse;
 #[cfg(feature = "mesh")]
+/// Contains the [`Mesh`] structure and related methods
 pub mod mesh;
 
+/// Contains the included shapes
 pub mod prelude {
     #[cfg(feature = "point")]
     pub use crate::point::Point;
